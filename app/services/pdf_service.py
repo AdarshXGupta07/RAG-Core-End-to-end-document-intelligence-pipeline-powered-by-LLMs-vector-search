ecf659a52d494a70bc858dbbb000508aa2862b3c
Adarsh Gupta <adarxxxshgupta@gmail.com>
2026-06-22 11:26:05 +0530

    fix: pdf_service.py update

--- app/services/pdf_service.py
+++ app/services/pdf_service.py
@@ -1,7 +1,12 @@
 import io
+import re
 from pdfminer.high_level import extract_text
 
 def extract_text_from_pdf(file_bytes: bytes):
     pdf_file = io.BytesIO(file_bytes)
     text = extract_text(pdf_file)
+    
+    # Double character fix: "wwoorrdd" → "word"
+    text = re.sub(r'(.)\1+', r'\1', text)
+    
     return text
